e6f800904ca34751c5e9eb92cc8adde4a55e408b
Tyson-Chicken-Nuggets <36246821+Tyson-Chicken-Nuggets@users.noreply.github.com>
2018-07-14 22:45:31 -0500

    ravioli ravioli append now in the codeioli

--- Loaf Bot/moderation.py
+++ Loaf Bot/moderation.py
@@ -143,7 +143,9 @@ class Moderation:
 			await ctx.send('Just delete it yourself, silly')
 		else:
 			message_limit = amount + 1
-			self.bot.messages = await ctx.message.channel.history(limit=message_limit).flatten()
+			self.bot.messages = []
+			async for message in ctx.message.channel.history(limit=message_limit):
+				self.bot.messages.append(message.id)
 			await ctx.message.channel.purge(limit=message_limit, bulk=True)
 			message = await ctx.send(f':white_check_mark: **{str(amount)}** messages deleted', delete_after=5)
 

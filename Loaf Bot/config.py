a8d9a1d3c836545aba25b1d4084ce50a7847c51c
Tyson-Chicken-Nuggets <36246821+Tyson-Chicken-Nuggets@users.noreply.github.com>
2018-07-15 21:41:05 -0500

    this should work

--- Loaf Bot/config.py
+++ Loaf Bot/config.py
@@ -24,10 +24,10 @@ class Config:
 Respond with a number from the list, or 'exit' to close the menu.```'''
 		)
 
-		def check(user):
-			user == ctx.message.author
+		def check(m):
+			m.content == '1' or m.content == '2' or m.content == '3' or m.content == '4' or m.content == '5' or m.content == '6' and m.channel == ctx.message.channel
 		try:
-			user = await self.bot.wait_for('message', timeout=30.0, check=check)
+			msg = await self.bot.wait_for('message', timeout=30.0, check=check)
 		except asyncio.TimeoutError:
 			await ctx.send('Menu closed')
 		else:

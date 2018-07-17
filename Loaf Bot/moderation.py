a00fb245ce0b8ec7f8df20863e42d782a28a2db0
Tyson-Chicken-Nuggets <36246821+Tyson-Chicken-Nuggets@users.noreply.github.com>
2018-07-16 23:36:51 -0500

    changed for multiple guild support

--- Loaf Bot/moderation.py
+++ Loaf Bot/moderation.py
@@ -15,17 +15,20 @@ class Moderation:
 		"""kicks inactive users"""
 		if time == 'init':
 			for member in ctx.guild.members:
-				update_time(self.bot, member.id)
+				update_time(self.bot, ctx.guild, member.id)
 			get_times(self.bot, ctx.guild)
 		else:
 			try:
 				weeks = int(time)
 				prune_members(self.bot, ctx, weeks)
 				for member in self.bot.pruned:
-					await ctx.guild.kick(member, reason='Pruned due to inactivity')
-					await ctx.send(f'Pruned {member.display_name} due to inactivity')
-					await member.create_dm()
-					await member.dm_channel.send(f'You were kicked from {ctx.guild.name} due to inactivity')
+					try:
+						await ctx.guild.kick(member, reason='Pruned due to inactivity')
+						await ctx.send(f'Pruned {member.display_name} due to inactivity')
+						await member.create_dm()
+						await member.dm_channel.send(f'You were kicked from {ctx.guild.name} due to inactivity')
+					except:
+						pass
 			except:
 				await ctx.send('Usage is `>prune <time in weeks>` or `>prune init` to restart/set the timer')
 			
